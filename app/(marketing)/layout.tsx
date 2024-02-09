4bd7ff6c92875958fdcc5c701ed1fa4958628f90
Eldana <fineshine6718@gmail.com>
2024-02-09 18:02:16 +0600

    first commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { NavBar } from "./_component/navbar";
+
+const MarketingLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="h-full">
+      <NavBar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
